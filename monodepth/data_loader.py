5152143914a0946f47cae246b41c4451c82caaf6
Dominik Straub <dominikstraub@yahoo.com>
2018-11-02 10:58:31 +0100

    Copy data_loader from MonoDepth-PyTorch repo

--- monodepth/data_loader.py
+++ monodepth/data_loader.py
@@ -1,1 +1,38 @@
- 
+import os
+from PIL import Image
+
+from torch.utils.data import Dataset
+
+
+class KittiLoader(Dataset):
+    def __init__(self, root_dir, mode, transform=None):
+        left_dir = os.path.join(root_dir, 'image_02/data/')
+        self.left_paths = sorted([os.path.join(left_dir, fname) for fname\
+                           in os.listdir(left_dir)])
+        if mode == 'train':
+            right_dir = os.path.join(root_dir, 'image_03/data/')
+            self.right_paths = sorted([os.path.join(right_dir, fname) for fname\
+                                in os.listdir(right_dir)])
+            assert len(self.right_paths) == len(self.left_paths)
+        self.transform = transform
+        self.mode = mode
+
+
+    def __len__(self):
+        return len(self.left_paths)
+
+    def __getitem__(self, idx):
+        left_image = Image.open(self.left_paths[idx])
+        if self.mode == 'train':
+            right_image = Image.open(self.right_paths[idx])
+            sample = {'left_image': left_image, 'right_image': right_image}
+
+            if self.transform:
+                sample = self.transform(sample)
+                return sample
+            else:
+                return sample
+        else:
+            if self.transform:
+                left_image = self.transform(left_image)
+            return left_image 
