14198ec8197f54c65c87bedf3b9a0a2832ce0cf8
Dominik Straub <dominikstraub@yahoo.com>
2018-11-02 14:44:11 +0100

    First attempts at implementing MonodepthLoss

--- monodepth/data_loader.py
+++ monodepth/data_loader.py
@@ -1,10 +1,13 @@
 import os
 from PIL import Image
 
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader, ConcatDataset
+from transforms import image_transforms
 
 
 class KittiLoader(Dataset):
+    """ DataSet that reads a single Kitti sequence
+    """
     def __init__(self, root_dir, mode, transform=None):
         left_dir = os.path.join(root_dir, 'image_02/data/')
         self.left_paths = sorted([os.path.join(left_dir, fname) for fname\
@@ -36,3 +39,60 @@ class KittiLoader(Dataset):
             if self.transform:
                 left_image = self.transform(left_image)
             return left_image 
+
+        
+def prepare_dataloader(data_directory, mode, augment_parameters=[0.8, 1.2, 0.5, 2.0, 0.8, 1.2],
+                       do_augmentation=True, batch_size=256, size=(256, 512), num_workers=1):
+    """ Prepares a DataLoader that loads multiple Kitti sequences
+    
+    Parameters
+    ----------
+    
+    data_directory : str
+        folder that contains multiple subfolders with Kitti sequences
+        
+    mode : str
+        'test' or 'train'
+    
+    augment_parameters : list
+        parameters for the data augmentation
+    
+    do_augmentation : bool
+        decides if data are augmented
+    
+    batch_size : int
+        number of images per batch
+        
+    num_workers : int
+        number of GPUs
+        
+    Returns
+    _______
+    
+    n_img : int
+        total number of images
+        
+    loader : torch.utils.data.DataLoader
+        data loader
+    """
+    data_dirs = os.listdir(data_directory)
+    data_transform = image_transforms(
+        mode=mode,
+        augment_parameters=augment_parameters,
+        do_augmentation=do_augmentation,
+        size = size)
+    datasets = [KittiLoader(os.path.join(data_directory,
+                            data_dir), mode, transform=data_transform)
+                            for data_dir in data_dirs]
+    dataset = ConcatDataset(datasets)
+    n_img = len(dataset)
+    print('Use a dataset with', n_img, 'images')
+    if mode == 'train':
+        loader = DataLoader(dataset, batch_size=batch_size,
+                            shuffle=True, num_workers=num_workers,
+                            pin_memory=True)
+    else:
+        loader = DataLoader(dataset, batch_size=batch_size,
+                            shuffle=False, num_workers=num_workers,
+                            pin_memory=True)
+    return n_img, loader
