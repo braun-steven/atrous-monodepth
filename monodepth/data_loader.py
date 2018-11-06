71a997612db2e5e0d6d2bc0126180097bdbe1cdb
Dominik Straub <dominikstraub@yahoo.com>
2018-11-06 10:43:58 +0100

    Add properly documented version of MonodepthLoss

--- monodepth/data_loader.py
+++ monodepth/data_loader.py
@@ -2,24 +2,34 @@ import os
 from PIL import Image
 
 from torch.utils.data import Dataset, DataLoader, ConcatDataset
-from transforms import image_transforms
+from .transforms import image_transforms
 
 
 class KittiLoader(Dataset):
-    """ DataSet that reads a single Kitti sequence
+    """ DataSet that reads a single Kitti sequence.
+        Can be accessed like a list.
+        If transform is specified, the transform is applied before returning an element.
+        If mode='train', each element is a dict containing 'left_image' and 'right_image'
     """
+
     def __init__(self, root_dir, mode, transform=None):
+        """ Setup a Kitti sequence dataset.
+
+        Args:
+            root_dir: data directory. should contain 'image_02' (left) and 'image_03' (right) subfolders
+            mode: 'train' or 'test'
+            transform: a torchvision.transforms type transform
+        """
         left_dir = os.path.join(root_dir, 'image_02/data/')
-        self.left_paths = sorted([os.path.join(left_dir, fname) for fname\
-                           in os.listdir(left_dir)])
+        self.left_paths = sorted([os.path.join(left_dir, fname) for fname \
+                                  in os.listdir(left_dir)])
         if mode == 'train':
             right_dir = os.path.join(root_dir, 'image_03/data/')
-            self.right_paths = sorted([os.path.join(right_dir, fname) for fname\
-                                in os.listdir(right_dir)])
+            self.right_paths = sorted([os.path.join(right_dir, fname) for fname \
+                                       in os.listdir(right_dir)])
             assert len(self.right_paths) == len(self.left_paths)
         self.transform = transform
         self.mode = mode
-
 
     def __len__(self):
         return len(self.left_paths)
@@ -38,52 +48,38 @@ class KittiLoader(Dataset):
         else:
             if self.transform:
                 left_image = self.transform(left_image)
-            return left_image 
+            return left_image
 
-        
+
 def prepare_dataloader(data_directory, mode, augment_parameters=[0.8, 1.2, 0.5, 2.0, 0.8, 1.2],
                        do_augmentation=True, batch_size=256, size=(256, 512), num_workers=1):
     """ Prepares a DataLoader that loads multiple Kitti sequences
     
-    Parameters
-    ----------
+    Args:
     
-    data_directory : str
-        folder that contains multiple subfolders with Kitti sequences
+        data_directory: (str) folder that contains multiple subfolders with Kitti sequences
+        mode: (str) 'test' or 'train'
+        augment_parameters: list of parameters for the data augmentation
+        do_augmentation: decides if data are augmented
+        batch_size: number of images per batch
+        num_workers: number of GPUs
         
-    mode : str
-        'test' or 'train'
-    
-    augment_parameters : list
-        parameters for the data augmentation
-    
-    do_augmentation : bool
-        decides if data are augmented
-    
-    batch_size : int
-        number of images per batch
-        
-    num_workers : int
-        number of GPUs
-        
-    Returns
-    _______
-    
-    n_img : int
-        total number of images
-        
-    loader : torch.utils.data.DataLoader
-        data loader
+    Returns:
+        n_img : int
+            total number of images
+
+        loader : torch.utils.data.DataLoader
+            data loader
     """
     data_dirs = os.listdir(data_directory)
     data_transform = image_transforms(
         mode=mode,
         augment_parameters=augment_parameters,
         do_augmentation=do_augmentation,
-        size = size)
+        size=size)
     datasets = [KittiLoader(os.path.join(data_directory,
-                            data_dir), mode, transform=data_transform)
-                            for data_dir in data_dirs]
+                                         data_dir), mode, transform=data_transform)
+                for data_dir in data_dirs]
     dataset = ConcatDataset(datasets)
     n_img = len(dataset)
     print('Use a dataset with', n_img, 'images')
