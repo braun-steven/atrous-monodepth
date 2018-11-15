2ff76c59505d2531c4e85e68e0dd896188b34215
Dominik Straub <dominikstraub@yahoo.com>
2018-11-15 14:59:07 +0100

    Revert filenames-files to *.jpg

--- resources/filenames/selectfiles.py
+++ resources/filenames/selectfiles.py
@@ -2,16 +2,16 @@ with open("scene_0001_train.txt", "w") as out:
     with open("kitti_train_files.txt") as file:
         for line in file:
             if "2011_09_26_drive_0001_sync" in line:
-                out.write(" ".join([s[:-4] + ".png" for s in line.split()]) + "\n")
+                out.write(line)
 
 with open("scene_0001_test.txt", "w") as out:
     with open("kitti_test_files.txt") as file:
         for line in file:
             if "2011_09_26_drive_0001_sync" in line:
-                out.write(" ".join([s[:-4] + ".png" for s in line.split()]) + "\n")
+                out.write(line)
 
 with open("scene_0001_val.txt", "w") as out:
     with open("kitti_val_files.txt") as file:
         for line in file:
             if "2011_09_26_drive_0001_sync" in line:
-                out.write(" ".join([s[:-4] + ".png" for s in line.split()]) + "\n")
+                out.write(line)
