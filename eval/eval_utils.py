3b8845879bf1b3d8fd4378bef7412cca83015689
Steven Lang <steven.lang.mz@gmail.com>
2018-11-16 00:34:09 +0100

    Replace python2 print

--- eval/eval_utils.py
+++ eval/eval_utils.py
@@ -85,8 +85,8 @@ def read_file_data(files, data_root):
         else:
             num_probs += 1
             print('{} missing'.format(data_root + im))
-            
-    print num_probs, 'files missing'
+
+    print(num_probs, 'files missing')
 
     return gt_files, gt_calib, im_sizes, im_files, cams
 
