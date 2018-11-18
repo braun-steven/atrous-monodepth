572b445946f64bcaffbad25bb76dd4b8d877a803
Steven Lang <steven.lang.mz@gmail.com>
2018-11-18 22:01:23 +0100

    Add loss tracking in experiment

--- experiment.py
+++ experiment.py
@@ -7,12 +7,15 @@ import time
 
 import torch
 
+from evaluator import Evaluator
 from monolab.data_loader import prepare_dataloader
 from monolab.loss import MonodepthLoss
 from monolab.networks.backbone import Backbone
 from monolab.networks.resnet_models import Resnet18_md, Resnet50_md
 from monolab.networks.deeplab.deeplabv3plus import DeepLabv3Plus, DCNNType
 import logging
+
+from monolab.networks.test_model import TestModel
 
 logger = logging.getLogger(__name__)
 
@@ -29,6 +32,16 @@ class Experiment:
 
     def __init__(self, args):
         self.args = args
+
+        self.loss_names = dict(
+            full="monodepth-loss",
+            images="image-loss",
+            disp_gp="disparity-gradient-loss",
+            lr_consistency="lr-consistency-loss",
+        )
+
+        # Setup Evaluator
+        self.eval = Evaluator(metric_names=list(self.loss_names.values()), args=args)
 
         # Set up model
         self.device = args.device
@@ -89,29 +102,23 @@ class Experiment:
         Returns:
             None
         """
-        losses = []
-        val_losses = []
+        # Store the best validation loss
         best_val_loss = float("Inf")
 
-        running_val_loss = 0.0
-        self.model.eval()
-        for data in self.val_loader:
-            data = to_device(data, self.device)
-            left = data["left_image"]
-            right = data["right_image"]
-            disps = self.model(left)
-            loss = self.loss_function(disps, [left, right])
-            val_losses.append(loss.item())
-            running_val_loss += loss.item()
-
-        running_val_loss /= self.val_n_img / self.args.batch_size
-        logging.info("Initial val. loss: {}".format(running_val_loss))
-
-        for epoch in range(self.args.epochs):
+        # Start training
+        for epoch in range(1, self.args.epochs + 1):
+            # Adjust learning rate if flag is set
             if self.args.adjust_lr:
                 adjust_learning_rate(self.optimizer, epoch, self.args.learning_rate)
+
             c_time = time.time()
+
+            # Init training loss
             running_loss = 0.0
+            running_image_loss = 0.0
+            running_disp_gradient_loss = 0.0
+            running_lr_loss = 0.0
+
             self.model.train()
             for data in self.loader:
                 # Load data
@@ -122,43 +129,88 @@ class Experiment:
                 # One optimization iteration
                 self.optimizer.zero_grad()
                 disps = self.model(left)
-                loss = self.loss_function(disps, [left, right])
+                loss, image_loss, disp_gradient_loss, lr_loss = self.loss_function(
+                    disps, [left, right]
+                )
                 loss.backward()
                 self.optimizer.step()
-                losses.append(loss.item())
 
+                # Collect training loss
                 running_loss += loss.item()
+                running_image_loss += image_loss.item()
+                running_disp_gradient_loss += disp_gradient_loss.item()
+                running_lr_loss += lr_loss.item()
 
+            # Init validation loss
             running_val_loss = 0.0
+            running_val_image_loss = 0.0
+            running_val_disp_gradient_loss = 0.0
+            running_val_lr_loss = 0.0
+
             self.model.eval()
             for data in self.val_loader:
                 data = to_device(data, self.device)
                 left = data["left_image"]
                 right = data["right_image"]
                 disps = self.model(left)
-                loss = self.loss_function(disps, [left, right])
-                val_losses.append(loss.item())
+                loss, image_loss, disp_gradient_loss, lr_loss = self.loss_function(
+                    disps, [left, right]
+                )
+
+                # Collect validation loss
                 running_val_loss += loss.item()
+                running_val_image_loss += image_loss.item()
+                running_val_disp_gradient_loss += disp_gradient_loss.item()
+                running_val_lr_loss += lr_loss.item()
 
             # Estimate loss per image
-            running_loss /= self.n_img / self.args.batch_size
-            running_val_loss /= self.val_n_img / self.args.batch_size
-            logging.info(
-                "Epoch: {}, train_loss: {}, val_loss: {}, time: {} s".format(
-                    epoch + 1,
-                    round(running_loss, 4),
-                    round(running_val_loss, 4),
-                    round(time.time() - c_time, 3),
+            running_loss /= self.n_img
+            running_image_loss /= self.n_img
+            running_disp_gradient_loss /= self.n_img
+            running_lr_loss /= self.n_img
+            running_val_loss /= self.val_n_img
+            running_image_loss /= self.val_n_img
+            running_disp_gradient_loss /= self.val_n_img
+            running_lr_loss /= self.val_n_img
+
+            # Update best loss
+            if running_val_loss < best_val_loss:
+                best_val_loss = running_val_loss
+
+            logger.info(
+                "Epoch [{}/{}] time: {} s".format(
+                    epoch + 1, self.args.epochs, round(time.time() - c_time, 3)
                 )
             )
-            self.save(self.args.model_path[:-4] + "_last.pth")
-            if running_val_loss < best_val_loss:
-                self.save(self.args.model_path[:-4] + "_cpt.pth")
-                best_val_loss = running_val_loss
-                logging.info("Model saved")
+            self.eval.add_epoch_metric(
+                epoch=epoch,
+                train_metric=running_loss,
+                val_metric=running_val_loss,
+                metric_name=self.loss_names["full"],
+            )
+            self.eval.add_epoch_metric(
+                epoch=epoch,
+                train_metric=running_image_loss,
+                val_metric=running_val_image_loss,
+                metric_name=self.loss_names["images"],
+            )
+            self.eval.add_epoch_metric(
+                epoch=epoch,
+                train_metric=running_disp_gradient_loss,
+                val_metric=running_val_disp_gradient_loss,
+                metric_name=self.loss_names["disp_gp"],
+            )
+            self.eval.add_epoch_metric(
+                epoch=epoch,
+                train_metric=running_lr_loss,
+                val_metric=running_val_lr_loss,
+                metric_name=self.loss_names["lr_consistency"],
+            )
+
+            self.eval.add_checkpoint(model=self.model, val_loss=running_val_loss)
 
         logging.info("Finished Training. Best loss: {}".format(best_val_loss))
-        self.save(self.args.model_path)
+        self.eval.save()
 
     def save(self, path):
         torch.save(self.model.state_dict(), path)
@@ -282,6 +334,8 @@ def get_model(model: str, n_input_channels=3) -> Backbone:
         out_model = Resnet18_md(num_in_layers=n_input_channels)
     elif model == "resnet50_md":
         out_model = Resnet50_md(num_in_layers=n_input_channels)
+    elif model == "testnet":
+        out_model = TestModel(n_in_layers=n_input_channels)
     # elif and so on and so on
     else:
         raise NotImplementedError("Unknown model type")
@@ -302,7 +356,7 @@ def setup_logging(filename: str = "monolab.log", level: str = "INFO"):
 
     logging.basicConfig(
         level=logging.getLevelName(level.upper()),
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        format="%(asctime)s - %(levelname)s - %(message)s",
         handlers=[
             logging.StreamHandler(stream=sys.stdout),
             logging.FileHandler(filename=filename),
