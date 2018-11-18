48f930824b36c9f3d3861d47622ea5e4f6c359b7
Fabian Kessler <fa.envision@googlemail.com>
2018-11-18 12:57:09 +0100

    first version of the evaluation classes, to evaluate on both ground truth data (eval_kitti_gt.py) and velodyne laser range scanner data (eval_eigensplit.py)

--- eval/eval_utils.py
+++ eval/eval_utils.py
@@ -1,8 +1,4 @@
 import numpy as np
-import os
-import cv, cv2
-from collections import Counter
-
 
 def compute_errors(gt, pred):
     """ Evaluates the predicted depth data on ground truth
@@ -38,57 +34,6 @@ def compute_errors(gt, pred):
     sq_rel = np.mean(((gt - pred)**2) / gt)
 
     return abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3
-
-
-def read_file_data(files, data_root):
-    """ Reads in the files for evaluation of ground truth data
-
-        Args:
-            files: a list of files
-            pred: numpy array (2D) of the predicted depth
-
-        Returns:
-            -abs_rel
-            -sq_rel
-            -rmse
-            -rmse_log
-            -a1
-            -a2x
-            -a3
-        """
-
-
-    gt_files = []
-    gt_calib = []
-    im_sizes = []
-    im_files = []
-    cams = []
-    num_probs = 0
-
-    for filename in files:
-        filename = filename.split()[0]
-        splits = filename.split('/')
-        camera_id = np.int32(splits[2][-1:])  # 2 is left, 3 is right
-        date = splits[0]
-        im_id = splits[4][:10]
-        file_root = '{}/{}'
-
-        im = filename
-        vel = '{}/{}/velodyne_points/data/{}.bin'.format(splits[0], splits[1], im_id)
-
-        if os.path.isfile(data_root + im):
-            gt_files.append(data_root + vel)
-            gt_calib.append(data_root + date + '/')
-            im_sizes.append(cv2.imread(data_root + im).shape[:2])
-            im_files.append(data_root + im)
-            cams.append(2)
-        else:
-            num_probs += 1
-            print('{} missing'.format(data_root + im))
-
-    print(num_probs, 'files missing')
-
-    return gt_files, gt_calib, im_sizes, im_files, cams
 
 
 
