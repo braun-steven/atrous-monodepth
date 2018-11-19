0d8f70b779677e33027b58dd2ed92aeff04ddcfb
Dominik Straub <dominikstraub@yahoo.com>
2018-11-19 17:22:04 +0100

    Change VGG implementation

--- monolab/networks/vgg_md.py
+++ monolab/networks/vgg_md.py
@@ -1,158 +1,170 @@
+#
+# Author : Alwyn Mathew
+#
+# Monodepth in pytorch(https://github.com/alwynmathew/monodepth-pytorch)
+#
+
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 
 
-class VGGMonodepth(torch.nn.Module):
-    def __init__(self, num_in_layers=3):
+class VGGMonodepth(nn.Module):  # vgg version
+    def __init__(self, num_in_layers, num_out_layers=2):
         super(VGGMonodepth, self).__init__()
 
-        # encoder
-        self.conv1 = conv_block(D_in=num_in_layers, D_out=32, k=7)  # h,w /2
-        self.conv2 = conv_block(D_in=32, D_out=64, k=5)  # h,w /4
-        self.conv3 = conv_block(D_in=64, D_out=128, k=3)  # h,w /8
-        self.conv4 = conv_block(D_in=128, D_out=256, k=3)  # h,w /16
-        self.conv5 = conv_block(D_in=256, D_out=512, k=3)  # h,w /32
-        self.conv6 = conv_block(D_in=512, D_out=512, k=3)  # h,w /64
-        self.conv7 = conv_block(D_in=512, D_out=512, k=3)  # h,w /128
-        # Decoder
-        self.upconv7 = upconv(D_in=512, D_out=512, k=3, scale=2)  # h,w / 64
-        self.iconv7 = iconv(D_in=1024, D_out=512, k=3)
-        self.upconv6 = upconv(D_in=512, D_out=512, k=3, scale=2)  # h,w / 32
-        self.iconv6 = iconv(D_in=1024, D_out=512, k=3)
-        self.upconv5 = upconv(D_in=512, D_out=256, k=3, scale=2)  # h,w / 16
-        self.iconv5 = iconv(D_in=512, D_out=256, k=3)
-        # -------------------------------------------------------------#
-        self.upconv4 = upconv(D_in=256, D_out=128, k=3, scale=2)  # h,w / 8
-        self.iconv4 = iconv(D_in=256, D_out=128, k=3)  # WRONG IN THE PAPER?
-        self.disp4 = disp(D_in=128, D_out=2, k=3, scale=2)
-        # -------------------------------------------------------------#
-        self.upconv3 = upconv(D_in=128, D_out=64, k=3, scale=2)  # h,w / 4
-        self.iconv3 = iconv(D_in=130, D_out=64, k=3)  # 64+64+2
-        self.disp3 = disp(D_in=64, D_out=2, k=3, scale=2)
-        # -------------------------------------------------------------#
-        self.upconv2 = upconv(D_in=64, D_out=32, k=3, scale=2)  # h,w / 2
-        self.iconv2 = iconv(D_in=66, D_out=32, k=3)  # 32+32+2
-        self.disp2 = disp(D_in=32, D_out=2, k=3, scale=2)
-        # -------------------------------------------------------------#
-        self.upconv1 = upconv(D_in=32, D_out=16, k=3, scale=2)  # h,w / 1
-        self.iconv1 = iconv(D_in=18, D_out=16, k=3)
-        self.disp1 = disp(D_in=16, D_out=2, k=3, scale=2)
+        self.output_nc = num_out_layers
+
+        self.downconv_1 = self.conv_down_block(num_in_layers, 32, 7)
+        self.downconv_2 = self.conv_down_block(32, 64, 5)
+        self.downconv_3 = self.conv_down_block(64, 128, 3)
+        self.downconv_4 = self.conv_down_block(128, 256, 3)
+        self.downconv_5 = self.conv_down_block(256, 512, 3)
+        self.downconv_6 = self.conv_down_block(512, 512, 3)
+        self.downconv_7 = self.conv_down_block(512, 512, 3)
+
+        self.upconv_7 = self.conv_up_block(512, 512)
+        self.upconv_6 = self.conv_up_block(512, 512)
+        self.upconv_5 = self.conv_up_block(512, 256)
+        self.upconv_4 = self.conv_up_block(256, 128)
+        self.upconv_3 = self.conv_up_block(128, 64)
+        self.upconv_2 = self.conv_up_block(64, 32)
+        self.upconv_1 = self.conv_up_block(32, 16)
+
+        self.conv_7 = self.conv_block(1024, 512)
+        self.conv_6 = self.conv_block(1024, 512)
+        self.conv_5 = self.conv_block(512, 256)
+        self.conv_4 = self.conv_block(256, 128)
+        self.conv_3 = self.conv_block(130, 64)
+        self.conv_2 = self.conv_block(66, 32)
+        self.conv_1 = self.conv_block(18, 16)
+
+        self.get_disp4 = self.disp_block(128)
+        self.get_disp3 = self.disp_block(64)
+        self.get_disp2 = self.disp_block(32)
+        self.get_disp1 = self.disp_block(16)
+
+    def conv_down_block(self, in_dim, out_dim, kernal):
+
+        conv_down_block = []
+        conv_down_block += [
+            nn.Conv2d(
+                in_dim,
+                out_dim,
+                kernel_size=kernal,
+                stride=1,
+                padding=int((kernal - 1) / 2),
+            ),
+            nn.BatchNorm2d(out_dim),
+            nn.ELU(),
+        ]  # h,w -> h,w
+        conv_down_block += [
+            nn.Conv2d(
+                out_dim,
+                out_dim,
+                kernel_size=kernal,
+                stride=2,
+                padding=int((kernal - 1) / 2),
+            ),
+            nn.BatchNorm2d(out_dim),
+            nn.ELU(),
+        ]  # h,w -> h/2,w/2
+
+        return nn.Sequential(*conv_down_block)
+
+    def conv_up_block(self, in_dim, out_dim):
+
+        conv_up_block = []
+        conv_up_block += [
+            nn.Conv2d(in_dim, out_dim, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_dim),
+            nn.ELU(),
+        ]  # h,w -> h,w
+
+        return nn.Sequential(*conv_up_block)
+
+    def conv_block(self, in_dim, out_dim):
+
+        conv_up_block = []
+        conv_up_block += [
+            nn.Conv2d(in_dim, out_dim, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_dim),
+            nn.ELU(),
+        ]  # h,w -> h,w
+
+        return nn.Sequential(*conv_up_block)
+
+    def disp_block(self, in_dim):
+
+        disp_block = []
+        disp_block += [
+            nn.Conv2d(in_dim, self.output_nc, kernel_size=3, stride=1, padding=1),
+            nn.Sigmoid(),
+        ]  # h,w -> h,w
+
+        return nn.Sequential(*disp_block)
+
+    def upsample_(self, disp, ratio):
+
+        s = disp.size()
+        h = int(s[2])
+        w = int(s[3])
+        nh = h * ratio
+        nw = w * ratio
+        temp = nn.functional.upsample(disp, [nh, nw], mode="nearest")
+
+        return temp
 
     def forward(self, x):
+        # 3x256x512
+        conv_1 = self.downconv_1(x)  # 32x128x256
+        conv_2 = self.downconv_2(conv_1)  # 64x64x128
+        conv_3 = self.downconv_3(conv_2)  # 128x32x64
+        conv_4 = self.downconv_4(conv_3)  # 256x16x32
+        conv_5 = self.downconv_5(conv_4)  # 512x8x16
+        conv_6 = self.downconv_6(conv_5)  # 512x4x8
+        conv_7 = self.downconv_7(conv_6)  # 512x2x4
 
-        # encoder
-        x1 = self.conv1(x)  # conv1b
-        x2 = self.conv2(x1)  # conv2b
-        x3 = self.conv3(x2)  # conv3b
-        x4 = self.conv4(x3)  # conv4b
-        x5 = self.conv5(x4)  # conv5b
-        x6 = self.conv6(x5)  # conv6b
-        x7 = self.conv7(x6)  # conv7b
+        conv7_up = self.upsample_(conv_7, 2)  # 512x4x8
+        upconv_7 = self.upconv_7(conv7_up)  # 512x4x8
+        concat_7 = torch.cat([upconv_7, conv_6], 1)  # 1024x4x8
+        iconv_7 = self.conv_7(concat_7)  # 512x4x8
 
-        encoder = [x1, x2, x3, x4, x5, x6, x7]
+        iconv7_up = self.upsample_(iconv_7, 2)  # 512x8x16
+        upconv_6 = self.upconv_6(iconv7_up)  # 512x8x16
+        concat_6 = torch.cat([upconv_6, conv_5], 1)  # 1024x8x16
+        iconv_6 = self.conv_6(concat_6)  # 512x8x16
 
-        # decoder
-        upconv7 = self.upconv7(x7)
-        iconv7 = self.iconv7(torch.cat((upconv7, x6), 1))
+        iconv6_up = self.upsample_(iconv_6, 2)  # 512x16x32
+        upconv_5 = self.upconv_5(iconv6_up)  # 256x16x32
+        concat_5 = torch.cat([upconv_5, conv_4], 1)  # 512x16x32
+        iconv_5 = self.conv_5(concat_5)  # 256x16x32
 
-        upconv6 = self.upconv6(iconv7)
-        iconv6 = self.iconv6(torch.cat((upconv6, x5), 1))
+        iconv5_up = self.upsample_(iconv_5, 2)  # 256x32x64
+        upconv_4 = self.upconv_4(iconv5_up)  # 128x32x64
+        concat_4 = torch.cat([upconv_4, conv_3], 1)  # 256x32x64
+        iconv_4 = self.conv_4(concat_4)  # 128x32x64
+        self.disp4 = 0.3 * self.get_disp4(iconv_4)  # 2x32x64
+        udisp4 = self.upsample_(self.disp4, 2)  # 2x64x128
 
-        upconv5 = self.upconv5(iconv6)
-        iconv5 = self.iconv5(torch.cat((upconv5, x4), 1))
+        iconv4_up = self.upsample_(iconv_4, 2)  # 128x64x128
+        upconv_3 = self.upconv_3(iconv4_up)  # 64x64x128
+        concat_3 = torch.cat([upconv_3, conv_2, udisp4], 1)  # 130x64x128
+        iconv_3 = self.conv_3(concat_3)  # 64x64x128
+        self.disp3 = 0.3 * self.get_disp3(iconv_3)  # 2x64x128
+        udisp3 = self.upsample_(self.disp3, 2)  # 2x128x256
 
-        upconv4 = self.upconv4(iconv5)
-        iconv4 = self.iconv4(torch.cat((upconv4, x3), 1))
-        self.disp4_out = self.disp4(iconv4)
-        self.udisp4 = torch.nn.functional.interpolate(
-            self.disp4_out, scale_factor=2, mode="bilinear", align_corners=True
-        )
+        iconv3_up = self.upsample_(iconv_3, 2)  # 64x128x256
+        upconv_2 = self.upconv_2(iconv3_up)  # 32x128x256
+        concat_2 = torch.cat([upconv_2, conv_1, udisp3], 1)  # 66x128x256
+        iconv_2 = self.conv_2(concat_2)  # 32x128x256
+        self.disp2 = 0.3 * self.get_disp2(iconv_2)  # 2x128x256
+        udisp2 = self.upsample_(self.disp2, 2)  # 2x256x512
 
-        upconv3 = self.upconv3(iconv4)
-        iconv3 = self.iconv3(torch.cat((upconv3, x2, self.udisp4), 1))
-        self.disp3_out = self.disp3(iconv3)
-        self.udisp3 = torch.nn.functional.interpolate(
-            self.disp3_out, scale_factor=2, mode="bilinear", align_corners=True
-        )
+        iconv2_up = self.upsample_(iconv_2, 2)  # 32x256x512
+        upconv_1 = self.upconv_1(iconv2_up)  # 16x256x512
+        concat_1 = torch.cat([upconv_1, udisp2], 1)  # 18x256x512
+        iconv_1 = self.conv_1(concat_1)  # 16x256x512
+        self.disp1 = 0.3 * self.get_disp1(iconv_1)  # 2x256x512
 
-        upconv2 = self.upconv2(iconv3)
-        iconv2 = self.iconv2(torch.cat((upconv2, x1, self.udisp3), 1))
-        self.disp2_out = self.disp2(iconv2)
-        self.udisp2 = torch.nn.functional.interpolate(
-            self.disp2_out, scale_factor=2, mode="bilinear", align_corners=True
-        )
-
-        upconv1 = self.upconv1(iconv2)
-        iconv1 = self.iconv1(torch.cat((upconv1, self.udisp2), 1))
-        self.disp1_out = self.disp1(iconv1)
-
-        decoder = [iconv7, iconv6, iconv5, iconv4, iconv3, iconv2, iconv1]
-
-        return self.disp1_out, self.disp2_out, self.disp3_out, self.disp4_out
-
-
-class conv_block(torch.nn.Module):
-    def __init__(self, D_in, D_out, k):
-        super(conv_block, self).__init__()
-        p = np.floor((k - 1) / 2).astype(np.int32)
-        self.conv_layer = torch.nn.Conv2d(
-            in_channels=D_in, out_channels=D_out, kernel_size=k, stride=2, padding=p
-        )
-        self.conv_layerb = torch.nn.Conv2d(
-            in_channels=D_out, out_channels=D_out, kernel_size=k, stride=1, padding=p
-        )
-        self.normalize = torch.nn.BatchNorm2d(D_out)
-
-    def forward(self, x):
-        x = F.elu(self.conv_layer(x))
-        x = self.normalize(x)
-        x = F.elu(self.conv_layerb(x))
-        x = self.normalize(x)
-        return x
-
-
-class upconv(torch.nn.Module):
-    def __init__(self, D_in, D_out, k, scale):
-        super(upconv, self).__init__()
-        self.scale = scale
-        p = np.floor((k - 1) / 2).astype(np.int32)
-        self.conv = torch.nn.Conv2d(
-            in_channels=D_in, out_channels=D_out, kernel_size=k, stride=1, padding=p
-        )
-
-    def forward(self, x):
-        x = torch.nn.functional.interpolate(
-            x, scale_factor=self.scale, mode="bilinear", align_corners=True
-        )
-        x = self.conv(x)
-        return torch.nn.functional.elu(x)
-
-
-class iconv(torch.nn.Module):
-    def __init__(self, D_in, D_out, k):
-        super(iconv, self).__init__()
-        p = np.floor((k - 1) / 2).astype(np.int32)
-        self.conv = torch.nn.Conv2d(
-            in_channels=D_in, out_channels=D_out, kernel_size=k, stride=1, padding=p
-        )
-
-    def forward(self, x):
-        x = self.conv(x)
-        return torch.nn.functional.elu(x)
-
-
-class disp(torch.nn.Module):
-    def __init__(self, D_in, D_out, k, scale):
-        super(disp, self).__init__()
-        p = 1
-        self.conv = torch.nn.Conv2d(
-            in_channels=D_in, out_channels=D_out, kernel_size=k, stride=1, padding=p
-        )
-        self.normalize = torch.nn.BatchNorm2d(2)
-        self.sigmoid = torch.nn.Sigmoid()
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.normalize(x)
-        return 0.3 * self.sigmoid(x)
+        return [self.disp1, self.disp2, self.disp3, self.disp4]
