680e090538fe126394603dd5e146e0f54d39f809
Dominik Straub <dominikstraub@yahoo.com>
2018-12-02 14:10:02 +0100

    Move checkpoint loading into _get_model

--- experiment.py
+++ experiment.py
@@ -48,11 +48,6 @@ class Experiment:
         # Get the model
         self.model = self._get_model(args)
 
-        # Load pretrained model
-        if args.checkpoint:
-            logging.info(f"Loading pretrained model from {args.checkpoint}")
-            self.load(args.checkpoint)
-
         # Setup loss, optimizer and validation set
         self.loss_function = MonodepthLoss(
             device=self.device,
@@ -66,7 +61,7 @@ class Experiment:
         )
         logger.debug(f"Using optimizer: {self.optimizer}")
 
-        self.dataset_val = args.val_filenames_file.split('_')[0].split('/')[2]
+        self.dataset_val = args.val_filenames_file.split("_")[0].split("/")[2]
 
         # the validation loader is a train loader but without data augmentation!
         self.val_n_img, self.val_loader = prepare_dataloader(
@@ -80,7 +75,7 @@ class Experiment:
             batch_size=args.batch_size,
             size=(args.input_height, args.input_width),
             num_workers=args.num_workers,
-            dataset=self.dataset_val
+            dataset=self.dataset_val,
         )
         logging.info(f"Using a validation set with {self.val_n_img} images")
 
@@ -88,7 +83,7 @@ class Experiment:
         self.output_dir = args.output_dir
         self.input_height = args.input_height
         self.input_width = args.input_width
-        self.dataset_train = args.filenames_file.split('_')[0].split('/')[2]
+        self.dataset_train = args.filenames_file.split("_")[0].split("/")[2]
 
         self.n_img, self.loader = prepare_dataloader(
             root_dir=args.data_dir,
@@ -101,9 +96,11 @@ class Experiment:
             batch_size=args.batch_size,
             size=(args.input_height, args.input_width),
             num_workers=args.num_workers,
-            dataset=self.dataset_train
+            dataset=self.dataset_train,
         )
-        logging.info(f"Using a training data set from {self.dataset_train} with {self.n_img} images")
+        logging.info(
+            f"Using a training data set from {self.dataset_train} with {self.n_img} images"
+        )
 
         if "cuda" in self.device:
             torch.cuda.synchronize()
@@ -135,6 +132,11 @@ class Experiment:
                 args.model, sum(p.numel() for p in model.parameters())
             )
         )
+
+        # Load pretrained model
+        if args.checkpoint:
+            logging.info(f"Loading pretrained model from {args.checkpoint}")
+            self.load(args.checkpoint)
 
         self.multi_gpu = len(args.cuda_device_ids) > 1 or args.cuda_device_ids[0] == -1
 
