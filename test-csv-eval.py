8b5bd83cee810618dafe64f31c025eee77bdd4c6
Steven Lang <steven.lang.mz@gmail.com>
2019-01-17 14:14:11 +0100

    Update test csv eval script

--- test-csv-eval.py
+++ test-csv-eval.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 # Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--result-dir", default="results", type=str)
+parser.add_argument("--rm-run-suffix", default=None, type=str)
+parser.add_argument("--sort", action="store_true")
+
 parser.add_argument(
     "--runs",
     nargs="+",
@@ -25,21 +28,37 @@ for run in args.runs:
     run_files = glob.glob(gl)
     files += run_files
 
+print("Found runs:")
+for f in files:
+    print(f)
+
 # Load scores.csv as dataframes
 dfs = []
 for score_file in files:
     df = pd.read_csv(score_file, header=0, delimiter=",")
     df.drop(df.index[1], inplace=True)
     dfs.append(df)
-    
+
 # Postprocess and merge dataframes
 df = pd.concat(dfs)
 files = [f.split(":", maxsplit=1)[1][4:].split("/test/")[0] for f in files]
+suffix_idx = 0
+comp = files[0]
+
+
+if args.rm_run_suffix:
+    files = [f[len(args.rm_run_suffix):] for f in files]
 df["run"] = pd.Series(files, index=df.index)
 df.columns = [s.strip() for s in df.columns]
+df.drop(columns=["pp"], inplace=True)
 df = df[df["abs_rel"] < 5.0]
 
 # Print stats and overview 
-print(df.sort_values(by="abs_rel"))
+if args.sort:
+    print(df.sort_values(by="abs_rel"))
+else:
+    print(df)
+
+
 print(df.describe())
 
