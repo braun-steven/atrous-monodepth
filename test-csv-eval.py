81c27e020ffd4c179b958ddf71b3a6f79332be8d
Fabian Kessler <fa.envision@googlemail.com>
2019-01-20 11:46:50 +0100

    support of exporting to csv, such that it can easily be copied to results sheet online

--- test-csv-eval.py
+++ test-csv-eval.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--result-dir", default="results", type=str)
 parser.add_argument("--rm-run-suffix", default=None, type=str)
 parser.add_argument("--sort", action="store_true")
+parser.add_argument("--output_file", type = str)
 
 parser.add_argument(
     "--runs",
@@ -20,6 +21,8 @@ parser.add_argument(
 args = parser.parse_args()
 
 result_dir = args.result_dir
+
+outfile = args.output_file
 
 # Find all files specified by the glob list
 files = []
@@ -50,7 +53,7 @@ if args.rm_run_suffix:
     files = [f[len(args.rm_run_suffix):] for f in files]
 df["run"] = pd.Series(files, index=df.index)
 df.columns = [s.strip() for s in df.columns]
-df.drop(columns=["pp"], inplace=True)
+df.drop("pp", inplace=True)
 df = df[df["abs_rel"] < 5.0]
 
 # Print stats and overview 
@@ -61,4 +64,12 @@ else:
 
 
 print(df.describe())
+
+if outfile:
+    df.to_csv(outfile)
+
+
+
+
+
 
