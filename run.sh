90ea0b683ab1327ff52a542c5a6a39fe670a9272
Dominik Straub <dstraub@VISINF.LOCAL@lab08.visinf.local>
2019-01-25 15:03:54 +0100

    Update run shell

--- run.sh
+++ run.sh
@@ -1,17 +1,21 @@
 umask 0007
 
 #!/bin/bash
-./env/bin/python main.py \
-    --model resnet50_md \
-    --output-dir /visinf/projects_students/monolab/results \
+nohup ./env/bin/python main.py \
+    --model deeplab \
+    --output-stride 16 \
+    --encoder-dilations 1 1 1 2 \
+    --atrous-rates 1 3 5 7 \
+    --learning-rate 2e-4 \
     --epochs 50 \
-    --batch-size 32 \
-    --device cuda:0 \
+    --batch-size 16 \
+    --cuda-device-ids 1 \
     --data-dir /visinf/projects_students/monolab/data/kitti \
     --filenames-file resources/filenames/kitti_train_files.txt \
     --val-filenames-file resources/filenames/kitti_val_files.txt \
-    --use-multiple-gpu \
+    --test-filenames-file resources/filenames/kitti_stereo_2015_test_files.txt \
+    --eval kitti-gt \
     --log-level info \
-    --tag resnet50_md_slang 
-
+    --tag stride16_atrous-1-3-5-7_skips_dstraub \
+    --notify dominikstraub@yahoo.com &
 echo $! > run.pid
